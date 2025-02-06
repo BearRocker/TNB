d021b48ad362f62c070cb69f19f055915f6c4d54
BearRocker <32397248+BearRocker@users.noreply.github.com>
2025-02-06 22:43:43 +0500

    Delete Config.py

--- Config.py
+++
@@ -1,7 +0,0 @@
-bot_token = '5985261020:AAGaQZdQLmUqgX9m4JwkrMVe9znzCJ0tzYs'
-bot_channel_id = -1002205074541
-DB_login = 'postgres'
-DB_password = 88005553535
-DB_PORT = 5432
-DB_HOST = 'localhost'
-DB_NAME = 'TNBMAIN'
